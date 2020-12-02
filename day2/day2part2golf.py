18bd7e3fd70947bdededb15b889e497e72ad2c66
Violet Moore <Pyroan@users.noreply.github.com>
2020-12-01 22:44:48 -0800

    last golf i promise probably

--- day2/day2part2golf.py
+++ day2/day2part2golf.py
@@ -1,1 +1,1 @@
-print(sum([(p[int(n[0])-1]==o)^(p[int(n[1])-1]==o)for n,o,p in[(j.split('-'),k[0],l)for j,k,l in[x.split()for x in open('day2.txt').readlines()]]]))
+print(sum([(c[int(a.split('-')[0])-1]==o[0])^(c[int(a.split('-')[1])-1]==o[0])for a,o,c in[x.split()for x in open('day2.txt').readlines()]]))
