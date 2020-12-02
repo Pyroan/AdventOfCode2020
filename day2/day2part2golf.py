7d9e6e99289123863c6dceb1b58f6d14ca080cd5
Evan Schoenberger <evanjschoenberger@gmail.com>
2020-12-01 22:13:40 -0800

    golf even harder

--- day2/day2part2golf.py
+++ day2/day2part2golf.py
@@ -1,2 +1,2 @@
 # Just a small golf, as a snack.
-print(sum([(n[2][int(n[0][0])-1]==n[1])^(n[2][int(n[0][1])-1]==n[1])for n in[[m[0].split('-'),m[1][0],m[2]]for m in[x.split()for x in open('day2.txt').readlines()]]]))
+print(sum([(p[int(n[0])-1]==o)^(p[int(n[1])-1]==o)for n,o,p in[(m[0].split('-'),m[1][0],m[2])for m in[x.split()for x in open('day2.txt').readlines()]]]))
