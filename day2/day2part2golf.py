85c3fb3dafe0966d30bb790dadd2591ee2bd27ee
Violet Moore <Pyroan@users.noreply.github.com>
2020-12-01 22:07:33 -0800

    optimize golf

--- day2/day2part2golf.py
+++ day2/day2part2golf.py
@@ -1,2 +1,2 @@
 # Just a small golf, as a snack.
-print(sum([(n['p'][int(n['c'][0])-1]==n['t'])^(n['p'][int(n['c'][1])-1]==n['t'])for n in[{'c':m[0].split('-'),'t':m[1][0],'p':m[2]}for m in[x.split()for x in open('day2.txt').readlines()]]]))
+print(sum([(n[2][int(n[0][0])-1]==n[1])^(n[2][int(n[0][1])-1]==n[1])for n in[[m[0].split('-'),m[1][0],m[2]]for m in[x.split()for x in open('day2.txt').readlines()]]]))
