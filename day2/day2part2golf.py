d378840df1bec49a2fb9df5f130baa44aed14ab5
Evan Schoenberger <evanjschoenberger@gmail.com>
2020-12-02 10:38:40 -0800

    i'm an addict

--- day2/day2part2golf.py
+++ day2/day2part2golf.py
@@ -1,2 +1,2 @@
 # Just a small golf, as a snack.
-print(sum((c[int(a.split('-')[0])-1]==o[0])^(c[int(a.split('-')[1])-1]==o[0])for a,o,c in map(str.split,open('day2.txt').readlines())))
+print(sum((c[int(a.split('-')[0])-1]==o[0])^(c[int(a.split('-')[1])-1]==o[0])for a,o,c in map(str.split,open('day2.txt'))))
