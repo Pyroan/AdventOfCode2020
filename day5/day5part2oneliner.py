6425f53607060c0c7086a9f79ab68f5ae2b1f620
Violet Moore <Pyroan@users.noreply.github.com>
2020-12-04 21:59:34 -0800

    i didn't think I was gonna do it but day 5 golf

---
+++ day5/day5part2oneliner.py
@@ -0,0 +1,6 @@
+# TECHNICALLY not correct because it prints out a lot of junk I just can't figure out
+# how to isolate the first nonconsecutive number
+s=set(range(1024))-{int(l.replace('F','0').replace('B','1').replace('L','0').replace('R','1'),2)for l in open('day5.txt')}
+print([i for i in s if i-1 not in s and i+1 not in s])
+
+# like yeah you can collapse this into a oneliner but it would suck because I can't think of a way to not repeat s (filter saves you one but still).
