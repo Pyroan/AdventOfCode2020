521c047994c5f6747f38bfa6348470d9f93d7d18
Violet Moore <Pyroan@users.noreply.github.com>
2020-12-04 22:00:15 -0800

    correctness

--- day5/day5part2oneliner.py
+++ day5/day5part2oneliner.py
@@ -1,3 +1,3 @@
 # like yeah you can collapse this into a oneliner but it would suck because I can't think of a way to not repeat s (filter saves you one but still).
 s=set(range(1024))-{int(l.replace('F','0').replace('B','1').replace('L','0').replace('R','1'),2)for l in open('day5.txt')}
-print([i for i in s if i-1 not in s and i+1 not in s])
+print([i for i in s if i-1 not in s and i+1 not in s][0])
